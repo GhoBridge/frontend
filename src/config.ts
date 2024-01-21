730289f869c118230de1ab888a8bec01acad3015
Yash Goyal <yashgo0018@gmail.com>
2024-01-21 20:45:13 +0530

    added the contract addresses

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,12 @@ export const contractAddresses: Record<
   number,
   { ghoToken: `0x${string}`; bridgeContract: `0x${string}` }
 > = {
-  89: {
-    ghoToken: "0x",
-    bridgeContract: "0x",
+  11155111: {
+    ghoToken: "0xa91cb2eee15f7a7f932dbc4041841b374dd04cd2",
+    bridgeContract: "0x97d6e18bd712be37cc9173b162233607ae5245db",
+  },
+  80001: {
+    ghoToken: "0xedfd5c267de8379523757abaa4e271fb70e2257c",
+    bridgeContract: "0x007084a2f2c2f563071c953812701ece4cc500a7",
   },
 };
